57ccbc1b0a09a05e6b47a3879c8c1199684635d6
Sara Bermudez <bermudezalvarezsaraestefania@gmail.com>
2021-04-29 19:12:10 -0500

    Diseño de Carrito y cambios en Detalle Producto

---
+++ src/app/carrito.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "470px";
+    //document.getElementById("body").style.backgroundColor = "rgba(133, 133, 133, 0.253)";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    //document.getElementById("body").style.backgroundColor = "rgba(255, 255, 255, 0)";
+}
